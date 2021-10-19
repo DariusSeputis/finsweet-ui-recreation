92640ff786f94b610fbbd63501c9d32a578dda52
DariusSeputis <82835894+DariusSeputis@users.noreply.github.com>
2021-10-19 22:42:20 +0300

    added default stylings to index.css

--- 1_frontend/src/App.js
+++ 1_frontend/src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Header from './components/header/Header';
 
 function App() {
   return (
     <>
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path='/' />
+          <Route path='/about' />
+          <Route path='/services' />
+          <Route path='/blog' />
+          <Route path='/contacts' />
+        </Switch>
+      </Router>
     </>
   );
 }
